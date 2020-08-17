afcaa4309997f43c5e7f6c56a37e8ea85e023aa8
amine elabbadi <amineelabbadi@gmail.com>
2020-08-17 21:50:54 +0100

    route change

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('working..') })
     
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
